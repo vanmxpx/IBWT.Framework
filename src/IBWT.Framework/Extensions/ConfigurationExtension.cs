388e4cf2943662a9240f10a8fb80f1b02ce5c012
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2021-04-17 19:35:09 +0300

    Update settings

--- src/IBWT.Framework/Extensions/ConfigurationExtension.cs
+++ src/IBWT.Framework/Extensions/ConfigurationExtension.cs
@@ -14,21 +14,7 @@ namespace IBWT.Framework.Extentions
         public static IServiceCollection AddTelegramBot(
             this IServiceCollection services
         )
-        {
-            
-            var sp = services.BuildServiceProvider();
-            var config = sp.GetService<IConfiguration>();
-            var env = sp.GetService<IHostingEnvironment>();
-
-            if (env.IsDevelopment())
-            {
-                services.Configure<BotOptions>(config.GetSection("BotOptionsTest"));
-            }
-            else 
-            {
-                services.Configure<BotOptions>(config.GetSection("BotOptions"));
-            }
-            
+        {           
             return  services.AddTransient<TelegramBot>();
         }
 
